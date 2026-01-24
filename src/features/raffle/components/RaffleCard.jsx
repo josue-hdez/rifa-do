10bc5f4f87c191173000336ee93ade82eca5d15c
Josue Hernandez <josuehdez05232005@gmail.com>
2026-01-24 09:00:00 -0500

    Remove pause button from the raffle card component

--- src/features/raffle/components/RaffleCard.jsx
+++ src/features/raffle/components/RaffleCard.jsx
@@ -11,7 +11,6 @@ import { Badge } from "@/components/ui/badge";
 import { ItemDescription } from "@/components/ui/item";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Pause } from "lucide-react";
 
 const RaffleCard = ({ raffle }) => {
   return (
@@ -23,12 +22,9 @@ const RaffleCard = ({ raffle }) => {
           <ItemDescription>{raffle.description}</ItemDescription>
         </CardDescription>
       </CardHeader>
-      <CardFooter className="space-x-1.5">
-        <Button className="w-[70%]" asChild>
+      <CardFooter>
+        <Button className="w-full" asChild>
           <Link href={`/dashboard/raffles/${raffle.id}`}>Manage Raffle</Link>
-        </Button>
-        <Button className="w-[30%]" variant="outline">
-          <Pause />
         </Button>
       </CardFooter>
     </Card>
